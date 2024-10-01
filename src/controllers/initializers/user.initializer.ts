fb2d07cdfc6984eedf818101a9ae3ec90236904f
francisco-JGC <jjjchico1@gmail.com>
2024-09-30 22:23:44 -0600

    feat: create defaultUser

--- src/controllers/initializers/user.initializer.ts
+++ src/controllers/initializers/user.initializer.ts
@@ -1,7 +1,7 @@
 import { User } from '../../entities/user/user.entity'
 import { AppDataSource } from '../../config/database.config'
 import { getRoleByName } from '../role.controller'
-import { createUser } from '../user.controller'
+import { assignRoleToUser, createUser } from '../user.controller'
 import { handleError, handleSuccess } from '../types'
 
 export const createDefaultUsers = async () => {
@@ -24,7 +24,12 @@ export const createDefaultUsers = async () => {
         const role = await getRoleByName(user.rolename)
 
         if (role.success) {
-          createUser
+          const createdUser = await createUser({ ...user })
+          if (createdUser.success)
+            await assignRoleToUser({
+              userId: createdUser.data?.id || -1,
+              role_name: user.rolename
+            })
         }
       }
     }
